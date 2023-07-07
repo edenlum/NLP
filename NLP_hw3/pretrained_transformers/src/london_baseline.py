3b2f048473c16181e3f4f8b1b79bf05a69de741b
Eden Lumbroso <edenlum@gmail.com>
2023-07-07 10:43:08 +0000

    3d done

--- NLP_hw3/pretrained_transformers/src/london_baseline.py
+++ NLP_hw3/pretrained_transformers/src/london_baseline.py
@@ -2,3 +2,16 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+
+import sys
+import utils
+from tqdm import tqdm
+
+def all_london(eval_corpus_path):
+  predictions = ["London" \
+    for line in tqdm(open(eval_corpus_path, encoding='utf-8'))]
+  total, correct = utils.evaluate_places(eval_corpus_path, predictions)
+  print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
+
+if __name__ == "__main__":
+  all_london(sys.argv[1])
